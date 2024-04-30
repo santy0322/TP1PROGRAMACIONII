019b8b7fb9baa07de6a368e9fe9ed1e0c34e5af4
santy0322 <santy0322@gmail.com>
2024-04-30 19:38:22 -0300

    primer commit

---
+++ Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1PROGRAMACIONII
+{
+    public class Cliente
+    {
+        public string? RazonSocial { get; set; }
+
+        public string? Direccion { get; set; }
+
+        public string? CodigoPostal { get; set; }
+
+        public double Cuit { get; set; }
+
+        public string? CondicionTributaria { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public int Telefono { get; set; }
+        
+        public string? CorreoElectronico { get; set;}
+    }
+
+
+
+
+    }
+}
